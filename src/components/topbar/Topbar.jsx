d2607271d852ce5bdc86d1eac040b5c092ff3922
spython <emmanuelegbo01@gmail.com>
2023-11-09 10:38:11 +0100

    commit

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,10 +3,18 @@ export default function Topbar() {
   return (
     <div className='topbar'>
         <div className="topLeft"></div>
+        
         <div className="topCenter">
             <img src="/assets/chryst.png" alt="head pic" />
         </div>
+        
         <div className="topRight">
+          <div className="profile">
+            <i className="topIcon fa-solid fa-meteor fa-beat"></i>
+            <div className="options">
+                <span>Logout</span>
+            </div>
+          </div>
 
         </div>
     </div>
