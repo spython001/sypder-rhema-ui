5754c47bbf99eddec7e07282d5891f2da0da09e5
spython <emmanuelegbo01@gmail.com>
2023-11-09 08:17:22 +0100

    change directory of the pictures to assets and modified files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import QuotesPage from './components/qoutesPage/QuotesPage'
+//import QuotesPage from './components/qoutesPage/QuotesPage'
+import Topbar from './components/topbar/Topbar'
 
 function App() {
 
   return (
     <>
-    <QuotesPage/>
+    <Topbar/>
     </>
   )
 }
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import './topbar.scss'
+export default function Topbar() {
+  return (
+    <div className='topbar'>
+        <div className="topLeft"></div>
+        <div className="topCenter">
+            <img src="/assets/chryst.png" alt="head pic" />
+        </div>
+        <div className="topRight">
+
+        </div>
+    </div>
+  )
+}
