cfa3cefd9440f45fd9ddf7dc55cd430471dd7803
spython <emmanuelegbo01@gmail.com>
2023-11-13 14:14:12 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,32 @@ import Write from './pages/write/Write';
 import About from './pages/about/About';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
 
+  const user = true;
+
   return (
-    <>
-    <Topbar/>
-    <div className="container">
-      <Login/>
-    </div>
-    </>
+    <Router>
+      <Switch>
+        <Topbar/>
+        <div className="container">
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+
+          <Route path="/register">{user ? <Home /> : <Register /> }</Route>
+          <Route path="/login">{ user ? <Home /> : <Login /> }</Route>
+          
+          <Route path="/write">{ user ? <Write /> : <Register />}</Route>
+          <Route path='/about'>{ user ? <About/> : <Register/> }</Route>
+          <Route path="/post/:postId">
+            <Single />
+          </Route>
+        </div>
+      </Switch>
+    </Router>
   )
 }
 
