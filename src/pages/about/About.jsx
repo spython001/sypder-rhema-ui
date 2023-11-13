1aeea303a60e6555f15aa4219f84bff08895c53a
spython <emmanuelegbo01@gmail.com>
2023-11-13 12:48:03 +0100

    commit

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,24 +4,22 @@ import './about.scss'
 export default function About() {
   return (
     <div className='about'>
-      <Sidebar/>
+        <Sidebar/>
       
-      <div className="wrapper">
-        <div className="title">
-          <span className='spanTitle'>SPYDER RHEMA</span>
-        </div>
+        <div className="wrapper">
+            <span className='spanTitle'>SPYDER RHEMA</span>
         
-        <div className="spanContent">
-            <span>
-                See this as a working in progress
-                though filled with imperfections would entertain you
-            </span>
-            <span>
-                See this as a mini compendium of my thoughts
-                to allow you transverse through some parts of my world ...
-            </span>
+            <div className="spanContent">
+                <span>
+                    See this as a working in progress
+                    though filled with imperfections would entertain you
+                </span>
+                <span>
+                    See this as a mini compendium of my thoughts
+                    to allow you transverse through some parts of my world ...
+                </span>
+            </div>
         </div>
-      </div>
     </div>
   )
 }
